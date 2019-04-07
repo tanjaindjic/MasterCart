1bee64adceeab52d8f8b137dd819ef8cea7522de
tanjaindjic <mali.patuljko@gmail.com>
2019-04-07 21:20:16 +0200

    options, cart i inbox button dodati, ne vide se u simulatoru ali se vide na pravom telefonu

--- app/src/main/java/com/pma/mastercart/MainActivity.java
+++ app/src/main/java/com/pma/mastercart/MainActivity.java
@@ -3,6 +3,7 @@ package com.pma.mastercart;
 import android.graphics.Color;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,11 +14,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
-
+    private SearchView searchView;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,30 +37,6 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
-
-        ImageButton b1=new ImageButton(this);
-        b1.setImageResource(R.drawable.ic_cart);
-        b1.setBackgroundColor(Color.alpha(0));
-        Toolbar.LayoutParams l3=new Toolbar.LayoutParams(Toolbar.LayoutParams.WRAP_CONTENT, Toolbar.LayoutParams.WRAP_CONTENT);
-        l3.gravity=Gravity.END;
-        b1.setLayoutParams(l3);
-        b1.setPadding(30,0,0,0);
-        toolbar.addView(b1);
-
-        ImageButton b2=new ImageButton(this);
-        b2.setImageResource(R.drawable.ic_inbox);
-        b2.setBackgroundColor(Color.alpha(0));
-        l3.gravity=Gravity.END;
-        b2.setPadding(30,0,0,0);
-        b2.setLayoutParams(l3);
-        toolbar.addView(b2);
-
-        ImageButton b3=new ImageButton(this);
-        b3.setImageResource(R.drawable.ic_search);
-        b3.setBackgroundColor(Color.alpha(0));
-        l3.gravity=Gravity.END;
-        b3.setLayoutParams(l3);
-        toolbar.addView(b3);
 
 
 
@@ -108,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
+        /*Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();*/
         switch (item.getItemId()) {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
