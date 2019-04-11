16041a17c3da9e481385e8b21ca05f40d0d6cd47
tanjaindjic <mali.patuljko@gmail.com>
2019-04-11 09:19:33 +0200

    dodat edit profila

--- app/src/main/java/com/pma/mastercart/ProfileActivity.java
+++ app/src/main/java/com/pma/mastercart/ProfileActivity.java
@@ -1,11 +1,21 @@
 package com.pma.mastercart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class ProfileActivity extends AppCompatActivity {
+    private ImageButton edit_name;
+    private ImageButton edit_lastname;
+    private ImageButton edit_address;
+    private ImageButton edit_email;
+    private ImageButton edit_pass;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +24,50 @@ public class ProfileActivity extends AppCompatActivity {
         setSupportActionBar(back_toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        edit_name = (ImageButton)findViewById(R.id.edit_profile_name_button);
+        edit_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout ll = (LinearLayout)findViewById(R.id.edit_profile_name_layout);
+                ll.setVisibility(View.VISIBLE);
+            }
+        });
+
+        edit_lastname = (ImageButton)findViewById(R.id.edit_profile_lastname_button);
+        edit_lastname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout ll = (LinearLayout)findViewById(R.id.edit_profile_lastname_layout);
+                ll.setVisibility(View.VISIBLE);
+            }
+        });
+
+        edit_address = (ImageButton)findViewById(R.id.edit_profile_address_button);
+        edit_address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout ll = (LinearLayout)findViewById(R.id.edit_profile_address_layout);
+                ll.setVisibility(View.VISIBLE);
+            }
+        });
+
+        edit_email = (ImageButton)findViewById(R.id.edit_profile_email_button);
+        edit_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout ll = (LinearLayout)findViewById(R.id.edit_profile_email_layout);
+                ll.setVisibility(View.VISIBLE);
+            }
+        });
+
+        edit_pass = (ImageButton)findViewById(R.id.edit_profile_password_button);
+        edit_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout ll = (LinearLayout)findViewById(R.id.edit_profile_password_layout);
+                ll.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
