df559b9ac3f797e686d7f9e3f5cad4c5a7ffd52f
tanjaindjic <mali.patuljko@gmail.com>
2019-06-25 15:11:44 +0200

    upload doteran

--- app/src/main/java/com/pma/mastercart/ProfileActivity.java
+++ app/src/main/java/com/pma/mastercart/ProfileActivity.java
@@ -12,7 +12,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -22,7 +21,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -32,31 +30,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.pma.mastercart.asyncTasks.AddCategoryTask;
 import com.pma.mastercart.asyncTasks.GetUserTask;
 import com.pma.mastercart.asyncTasks.ProfilePictureTask;
 import com.pma.mastercart.asyncTasks.UpdateUserTask;
-import com.pma.mastercart.model.Category;
 import com.pma.mastercart.model.DTO.EditUserDTO;
 import com.pma.mastercart.model.User;
-
-import net.gotev.uploadservice.MultipartUploadRequest;
-import net.gotev.uploadservice.UploadNotificationConfig;
 
 import org.springframework.util.support.Base64;
 
 import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener  {
@@ -78,9 +60,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private Button saveChangesBtn;
     private static final int STORAGE_PERMITION_CODE = 123;
     private static final int PICK_IMAGE_REQUEST = 22;
-    private Uri filePath;
+    private String filePath;
     private Bitmap bitmap;
     private String imagePath;
+    private String file_extn;
 
 
     @Override
@@ -264,35 +247,64 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             if (resultCode == Activity.RESULT_OK) {
                 Uri selectedImage = data.getData();
 
-                String filePath = getPath(selectedImage);
-                String file_extn = filePath.substring(filePath.lastIndexOf(".") + 1);
+                filePath = getPath(selectedImage);
+                file_extn = filePath.substring(filePath.lastIndexOf(".") + 1);
+                Bitmap bm = BitmapFactory.decodeFile(filePath);
+                profile_thumbnail.setImageBitmap(bm);
 
-
-                try {
-                    if (file_extn.equals("img") || file_extn.equals("jpg") || file_extn.equals("jpeg") || file_extn.equals("gif") || file_extn.equals("png")) {
-                        Bitmap bm = BitmapFactory.decodeFile(filePath);
-                        profile_thumbnail.setImageBitmap(bm);
-                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                        byte[] imageBytes = baos.toByteArray();
-                        String encodedImage = Base64.encodeBytes(imageBytes);
-
-
-
-                        // Upload image to server
-                        SharedPreferences pref = this.getSharedPreferences(MainActivity.PREFS, 0); // 0 - for private mode
-                        Object[] objects = {encodedImage,  pref.getString("AuthToken", "")};
-                        new ProfilePictureTask().execute(objects);
-                    } else {
-                        //NOT IN REQUIRED FORMAT
-                    }
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
             }
     }
 
+    private int upload(){
+        try {
+            if (file_extn.equals("img") || file_extn.equals("jpg") || file_extn.equals("jpeg") || file_extn.equals("gif") || file_extn.equals("png")) {
+                Bitmap bm = BitmapFactory.decodeFile(filePath);
+                profile_thumbnail.setImageBitmap(bm);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] imageBytes = baos.toByteArray();
+                String encodedImage = Base64.encodeBytes(imageBytes);
+
+
+
+                // Upload image to server
+                SharedPreferences pref = this.getSharedPreferences(MainActivity.PREFS, 0); // 0 - for private mode
+                Object[] objects = {encodedImage,  pref.getString("AuthToken", "")};
+                new ProfilePictureTask().execute(objects);
+            } else {
+                //NOT IN REQUIRED FORMAT
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        String firstName = profile_firstName.getText().toString().trim();
+        String lastName = profile_lastName.getText().toString().trim();
+        String address = profile_address.getText().toString().trim();
+        String email = profile_email.getText().toString().trim();
+        String password = profile_password.getText().toString().trim();
+        String phone = profile_phone.getText().toString().trim();
+        if(email.isEmpty() || password.isEmpty()){
+            return 1;
+        }
+        EditUserDTO editUserDTO = new EditUserDTO(email, password, firstName, lastName, address, phone);
+        SharedPreferences pref = this.getSharedPreferences(MainActivity.PREFS, 0); // 0 - for private mode
+        Object[] objects = {editUserDTO,  pref.getString("AuthToken", "")};
+        AsyncTask<Object, Void, User> task = new UpdateUserTask().execute(objects);
+        User response = null;
+        try {
+            response = task.get();
+            if(response==null){
+                return 1;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -322,165 +334,9 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         return cursor.getString(column_index);
     }
 
-    private int uploadImage(String sourceFileUri){
-        int day, month, year;
-        int second, minute, hour;
-        GregorianCalendar date = new GregorianCalendar();
-
-        day = date.get(Calendar.DAY_OF_MONTH);
-        month = date.get(Calendar.MONTH);
-        year = date.get(Calendar.YEAR);
-
-        second = date.get(Calendar.SECOND);
-        minute = date.get(Calendar.MINUTE);
-        hour = date.get(Calendar.HOUR);
-
-        String name=(hour+""+minute+""+second+""+day+""+(month+1)+""+year);
-        String tag=name+".jpg";
-        String fileName = sourceFileUri.replace(sourceFileUri,tag);
-
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-        File sourceFile = new File(sourceFileUri);
-        int serverResponseCode = 0;
-
-        if (!sourceFile.isFile()) {
-
-            Log.e("uploadFile", "Source File not exist :"+sourceFileUri);
-
-            runOnUiThread(new Runnable() {
-                public void run() {
-
-                }
-            });
-
-            return 0;
-
-        }
-        else
-        {
-            try {
-
-                // open a URL connection to the Servlet
-                FileInputStream fileInputStream = new FileInputStream(sourceFile);
-                URL url = new URL(MainActivity.URL+"upload");
-
-                // Open a HTTP  connection to  the URL
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setDoInput(true); // Allow Inputs
-                conn.setDoOutput(true); // Allow Outputs
-                conn.setUseCaches(false); // Don't use a Cached Copy
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Connection", "Keep-Alive");
-                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-                conn.setRequestProperty("uploaded_file", fileName);
-
-                dos = new DataOutputStream(conn.getOutputStream());
-
-                dos.writeBytes(twoHyphens + boundary + lineEnd);
-                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
-                        + fileName + "\"" + lineEnd);
-
-                dos.writeBytes(lineEnd);
-
-
-
-
-                // create a buffer of  maximum size
-                bytesAvailable = fileInputStream.available();
-
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                buffer = new byte[bufferSize];
-
-                // read file and write it into form...
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                while (bytesRead > 0) {
-
-                    dos.write(buffer, 0, bufferSize);
-                    bytesAvailable = fileInputStream.available();
-                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                }
-
-                // send multipart form data necesssary after file data...
-                dos.writeBytes(lineEnd);
-                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-                // Responses from the server (code and message)
-                serverResponseCode = conn.getResponseCode();
-                String serverResponseMessage = conn.getResponseMessage();
-
-                Log.i("uploadFile", "HTTP Response is : "
-                        + serverResponseMessage + ": " + serverResponseCode);
-
-                if(serverResponseCode == 200){
-
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
-                                    +" C:/wamp/wamp/www/uploads";
-                           // Toast.makeText(this, "File Upload Complete.", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-
-                //close the streams //
-                fileInputStream.close();
-                dos.flush();
-                dos.close();
-
-            } catch (MalformedURLException ex) {
-
-                ex.printStackTrace();
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                       // Toast.makeText(this, "MalformedURLException", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
-            } catch (Exception e) {
-                e.printStackTrace();
-                Toast.makeText(this, "Got Exception : see logcat ", Toast.LENGTH_SHORT).show();
-                    }
-            }
-            return serverResponseCode;
-    }
 
     private int saveChanges() {
-        String firstName = profile_firstName.getText().toString().trim();
-        String lastName = profile_lastName.getText().toString().trim();
-        String address = profile_address.getText().toString().trim();
-        String email = profile_email.getText().toString().trim();
-        String password = profile_password.getText().toString().trim();
-        String phone = profile_phone.getText().toString().trim();
-        if(email.isEmpty() || password.isEmpty()){
-            return 1;
-        }
-        EditUserDTO editUserDTO = new EditUserDTO(email, password, firstName, lastName, address, phone);
-        SharedPreferences pref = this.getSharedPreferences(MainActivity.PREFS, 0); // 0 - for private mode
-        Object[] objects = {editUserDTO,  pref.getString("AuthToken", "")};
-        AsyncTask<Object, Void, User> task = new UpdateUserTask().execute(objects);
-        User response = null;
-        try {
-            response = task.get();
-            if(response==null){
-                return 1;
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        return 0;
+        return upload();
+
     }
 }
