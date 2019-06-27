e133b85002d824d114120d81bf770a564875a3bd
unknown <danijela.zelenovic@hotmail.com>
2019-06-27 16:08:10 +0200

    Izmjena i dopuna modela.

--- app/src/main/java/com/pma/mastercart/model/Conversation.java
+++ app/src/main/java/com/pma/mastercart/model/Conversation.java
@@ -7,14 +7,16 @@ import java.util.List;
 
 public class Conversation implements Parcelable {
     private Long id;
-    Shop reciever;
-    User sender;
+    Shop shop;
+    User initiator;
+    User receiver;
     List<Message> messages;
 
-    public Conversation(Long id, Shop receiver, User sender, List<Message> messages) {
+    public Conversation(Long id, Shop shop, User initiator, User receiver, List<Message> messages) {
         this.id = id;
-        this.reciever = receiver;
-        this.sender = sender;
+        this.shop = shop;
+        this.initiator = initiator;
+        this.receiver = receiver;
         this.messages = messages;
     }
 
@@ -28,20 +30,28 @@ public class Conversation implements Parcelable {
         this.id = id;
     }
 
-    public Shop getReciever() {
-        return reciever;
+    public Shop getShop() {
+        return shop;
     }
 
-    public void setReciever(Shop reciever) {
-        this.reciever = reciever;
+    public void setShop(Shop shop) {
+        this.shop = shop;
     }
 
-    public User getSender() {
-        return sender;
+    public User getInitiator() {
+        return initiator;
     }
 
-    public void setSender(User sender) {
-        this.sender = sender;
+    public void setInitiator(User initiator) {
+        this.initiator = initiator;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
     }
 
     public List<Message> getMessages() {
@@ -61,15 +71,17 @@ public class Conversation implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeValue(this.id);
-        dest.writeParcelable(this.reciever, flags);
-        dest.writeParcelable(this.sender, flags);
+        dest.writeParcelable(this.shop, flags);
+        dest.writeParcelable(this.initiator, flags);
+        dest.writeParcelable(this.receiver, flags);
         dest.writeTypedList(this.messages);
     }
 
     protected Conversation(Parcel in) {
         this.id = (Long) in.readValue(Long.class.getClassLoader());
-        this.reciever = in.readParcelable(Shop.class.getClassLoader());
-        this.sender = in.readParcelable(User.class.getClassLoader());
+        this.shop = in.readParcelable(Shop.class.getClassLoader());
+        this.initiator = in.readParcelable(User.class.getClassLoader());
+        this.receiver = in.readParcelable(User.class.getClassLoader());
         this.messages = in.createTypedArrayList(Message.CREATOR);
     }
 
